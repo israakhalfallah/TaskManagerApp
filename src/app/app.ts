9232c6407dd6d24f3297014c9ee89e95c87cf4d9
israa <israakhalaf62@gmail.com>
2025-11-11 10:22:50 +0100

    israa

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,30 +4,33 @@ import { Task } from "./task/task";
 import { Menu } from "./menu/menu";
 import { FrontEnd } from "./front-end/front-end";
 import { BackEnd } from "./back-end/back-end";
+import { LowerCasePipe, TitleCasePipe, UpperCasePipe ,DatePipe, DecimalPipe, CurrencyPipe, PercentPipe} from '@angular/common';
+import { TaskService } from './task-service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Task, Menu, FrontEnd, BackEnd],
+  imports: [RouterOutlet, Task, Menu, FrontEnd, BackEnd,UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,DecimalPipe,CurrencyPipe,PercentPipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('todolist');
+ i: number=0;
+ tasks: any[]=[]; 
+constructor(private taskService: TaskService) {}
+ngOnInit() {
+this.tasks = this.taskService.getTasks();
+}
  
- 
-  tasks = [
-{ id:'1',title: 'Learn Angular', done: false },
-{ id:'2',title: 'Build ToDoList App', done: false },
-{ id:'3', title: 'Celebrate!', done: true }
-];
-
 addNewTask(){
   this.tasks.push({ id:'1',title: 'New Task', done: false },)
 }
 deleteTask(ind:number){
 
-  this.tasks.splice(ind,1)
-
-
+  this.tasks.splice(ind,1);
 }
+today = new Date();
+price = 1234.56;
+percentage = 0.259;
 }
+
